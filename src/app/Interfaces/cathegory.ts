26fb89492b93bf3220b33541f950a91356dd444d
YOYOIJJKA <yoyoijjka@yandex.ru>
2023-11-21 03:29:29 +0300

    add priority and cathegory interfaces, fix task http service methods attached to cathegory and priority

---
+++ src/app/Interfaces/cathegory.ts
@@ -0,0 +1,4 @@
+export interface Cathegory {
+    id:number,
+    name:string
+}
--- src/app/Services/task-http-service.service.ts
+++ src/app/Services/task-http-service.service.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Task } from '../Interfaces/task';
-import {Observable } from "rxjs"
+import {Observable } from "rxjs";
+import { Priority } from '../Interfaces/priority';
+import { Cathegory } from '../Interfaces/cathegory';
 
 @Injectable({
   providedIn: 'root'
@@ -29,29 +31,29 @@ deleteTask (id:number):Observable<Task>
   return this.http.delete<Task>('http://localhost:3000/tasks/'+id)
 }
 //cathegories
-postCathegory (cathegory:string):Observable<string>
+postCathegory (cathegory:string):Observable<Cathegory>
 {
-  return this.http.post<string>('http://localhost:3000/cathegories/',cathegory)
+  return this.http.post<Cathegory>('http://localhost:3000/cathegories/',cathegory)
 }
-getCathegories ():Observable<string[]>
+getCathegories ():Observable<Cathegory[]>
 {
-  return this.http.get<string[]>('http://localhost:3000/cathegories/')
+  return this.http.get<Cathegory[]>('http://localhost:3000/cathegories/')
 }
-deleteCathegory (id:number):Observable<string>
+deleteCathegory (id:number):Observable<Cathegory>
 {
-  return this.http.delete<string>('http://localhost:3000/cathegories/'+id)
+  return this.http.delete<Cathegory>('http://localhost:3000/cathegories/'+id)
 }
 //priorities
-postPriority (priority:string):Observable<string>
+postPriority (priority:string):Observable<Priority>
 {
-  return this.http.post<string>('http://localhost:3000/priority/',priority)
+  return this.http.post<Priority>('http://localhost:3000/priority/',priority)
 }
-getPriorities ():Observable<string[]>
+getPriorities ():Observable<Priority[]>
 {
-  return this.http.get<string[]>('http://localhost:3000/priority/')
+  return this.http.get<Priority[]>('http://localhost:3000/priority/')
 }
-deletePriority (id:number):Observable<string>
+deletePriority (id:number):Observable<Priority>
 {
-  return this.http.delete<string>('http://localhost:3000/priority/'+id)
+  return this.http.delete<Priority>('http://localhost:3000/priority/'+id)
 }
 }
